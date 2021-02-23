eb942788fdfa90729ce76664308971bb10bda6ad
technicallyty <48813565+tytech3@users.noreply.github.com>
2021-02-23 12:58:36 -0800

    removed unnecessary type

--- x/upgrade/types/modulemanager.go
+++
@@ -1,9 +0,0 @@
-package types
-
-// MigrationMap - maps module names to their consensus versions
-type MigrationMap map[string]uint64
-
-// ModuleManager - interface to assist with module upgrades
-type ModuleManager interface {
-	GetConsensusVersion() MigrationMap
-}
